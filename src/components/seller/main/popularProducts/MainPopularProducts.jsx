bb3e2e64d55ee30dc2e8b0588c36a94ec0841451
dmstjd3256 <dmstjd3256@naver.com>
2022-09-23 09:07:56 +0900

    seller mainpage layout 구축

---
+++ src/components/seller/main/popularProducts/MainPopularProducts.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { WhiteWrapper } from '../../common/Box.style';
+
+export default function MainPopularProducts() {
+  return (
+    <WhiteWrapper width="100%" marginBottom="10px">
+      <div style={{ height: '600px' }}></div>
+    </WhiteWrapper>
+  );
+}
