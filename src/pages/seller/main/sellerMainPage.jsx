1a411379b693c69ae094f5a2526c3b8730d3dc22
dmstjd3256 <dmstjd3256@naver.com>
2022-09-27 10:52:06 +0900

    feat: seller 중첩 router 설정

--- src/pages/seller/main/sellerMainPage.jsx
+++ src/pages/seller/main/sellerMainPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import {
-  MainWrapper,
   PageCol,
   PageRow,
   RightWrapper,
 } from '../../../components/seller/common/Box.style';
-import SellerNavbar from '../../../components/seller/common/Navbar/SellerNavbar';
 import { SellerTitle } from '../../../components/seller/common/Title.style';
 import MainPopularProducts from '../../../components/seller/main/popularProducts/MainPopularProducts';
 import MainReviews from '../../../components/seller/main/reviews/MainReviews';
@@ -14,21 +12,18 @@ import MainUserManagement from '../../../components/seller/main/userManagement/M
 
 export default function SellerMainPage() {
   return (
-    <MainWrapper>
-      <SellerNavbar />
-      <RightWrapper>
-        <SellerTitle>Dashboard</SellerTitle>
-        <PageRow>
-          <PageCol width="calc(100% - 400px)" paddingRight="8px">
-            <MainUserManagement />
-            <MainSalesStatistics />
-          </PageCol>
-          <PageCol width="400px">
-            <MainPopularProducts />
-            <MainReviews />
-          </PageCol>
-        </PageRow>
-      </RightWrapper>
-    </MainWrapper>
+    <RightWrapper>
+      <SellerTitle>Dashboard</SellerTitle>
+      <PageRow>
+        <PageCol width="calc(100% - 400px)" paddingRight="8px">
+          <MainUserManagement />
+          <MainSalesStatistics />
+        </PageCol>
+        <PageCol width="400px">
+          <MainPopularProducts />
+          <MainReviews />
+        </PageCol>
+      </PageRow>
+    </RightWrapper>
   );
 }
--- src/pages/seller/promotion/sellerPromotionPage.jsx
+++ src/pages/seller/promotion/sellerPromotionPage.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import {
-  MainWrapper,
   PageCol,
   PageRow,
   RightWrapper,
 } from '../../../components/seller/common/Box.style';
-import SellerNavbar from '../../../components/seller/common/Navbar/SellerNavbar';
 import { SellerTitle } from '../../../components/seller/common/Title.style';
 
 export default function SellerPromotionPage() {
   return (
-    <MainWrapper>
-      <SellerNavbar />
-      <RightWrapper>
-        <SellerTitle>Promotion</SellerTitle>
-        <PageRow>
-          <PageCol width="calc(100% - 400px)"></PageCol>
-          <PageCol width="400px"></PageCol>
-        </PageRow>
-      </RightWrapper>
-    </MainWrapper>
+    <RightWrapper>
+      <SellerTitle>Promotion</SellerTitle>
+      <PageRow>
+        <PageCol width="calc(100% - 400px)"></PageCol>
+        <PageCol width="400px"></PageCol>
+      </PageRow>
+    </RightWrapper>
   );
 }
