db5a4ae0d877894da8d07caac914f870b7b914ac
이석원 <clalsw@seokwon-MacBookPro.local>
2022-09-28 09:36:48 +0900

    hotfix/네브바

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import MainIndexLayout from './pages/main/mainIndexLayout';
-import { SellerMain, SellerProducts, SellerPromotion } from './pages/seller';
-import SellerLoginPage from './pages/seller/account/login/sellerLoginPage';
 import SellerIndexLayout from './pages/seller/sellerIndexLayout';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* 셀러 Router */}
-        <Route exact path="/seller" element={<SellerMain />}></Route>
-        <Route exact path="/seller/login" element={<SellerLoginPage />}></Route>
-        <Route
-          exact
-          path="/seller/products"
-          element={<SellerProducts />}
-        ></Route>
-        <Route
-          exact
-          path="/seller/promotion"
-          element={<SellerPromotion />}
-        ></Route>
         <Route exact path="/*" element={<MainIndexLayout />} />
         {/* seller Router */}
         <Route exact path="/seller/*" element={<SellerIndexLayout />} />
