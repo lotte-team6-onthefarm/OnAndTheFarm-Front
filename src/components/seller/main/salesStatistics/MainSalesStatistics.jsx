21bea6660b8a98506907a0ff2da318c4c8aab643
dmstjd3256 <dmstjd3256@naver.com>
2022-09-29 10:38:59 +0900

    Seller 파일 정리

--- src/components/seller/main/salesStatistics/MainSalesStatistics.jsx
+++ src/components/seller/main/salesStatistics/MainSalesStatistics.jsx
@@ -5,7 +5,7 @@ import SubTitle from '../../common/SubTitle';
 
 export default function MainSalesStatistics() {
   return (
-    <WhiteWrapper width="100%" height="50vh">
+    <WhiteWrapper width="100%">
       <SubTitle color="#CABDFF" title="판매 통계" />
       <High></High>
     </WhiteWrapper>
