8af723b8baa6e8c06509203e5f12557ef7b9fbe7
이석원 <clalsw@seokwon-MacBookPro.local>
2022-09-29 17:08:24 +0900

    feat/searchInput

---
+++ src/components/common/SearchInput.style.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+const SearchInputDiv = styled.div`
+  position: relative;
+  width: ${props => (props.width ? props.width : '100%')};
+  margin: 20px auto;
+  display: flex;
+  align-items: center;
+  text-align: start;
+  svg {
+    position: absolute;
+    color: #fff;
+    right: 0;
+    background-color: #40aa54;
+    width: 40px;
+    height: 40px;
+    padding: 0 1rem;
+    border-radius: 100px;
+    cursor: pointer;
+  }
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  height: 40px;
+  padding: 0 80px 0 15px;
+  background-color: #f7f7f7;
+  border: none;
+  border-radius: 100px;
+`;
+
+export { SearchInputDiv, SearchInput };
