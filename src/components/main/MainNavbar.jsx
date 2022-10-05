a29365821f5ca957e212d1bb115a8fc51762e3eb
이석원 <clalsw@seokwon-MacBookPro.local>
2022-10-05 09:17:23 +0900

    feat/네브바구성 로그인페이지

--- src/components/main/MainNavbar.jsx
+++ src/components/main/MainNavbar.jsx
@@ -1,23 +1,54 @@
 import React from 'react';
-import { MenuItems } from './MenuItems';
-import { MenuIcons } from './MenuIcons';
 import onandthefarmlogo from '../../assets/Logo.svg';
 import { Link } from 'react-router-dom';
 import {
-  BlackLogoImg,
+  LogoImg,
   Navbar,
+  NavbarIcon,
+  NavbarIcons,
   NavbarMenu,
   NavbarMenuPtag,
+  NavbarSeller,
   NavbarUser,
 } from './MainNavbar.style';
+import { AiOutlineHeart, AiOutlineShoppingCart } from 'react-icons/ai';
 
 export default function MainNavbar(props) {
+  const MenuItems = [
+    {
+      title: '메인페이지',
+      url: '/',
+    },
+    {
+      title: '상품전제보기',
+      url: '/products',
+    },
+    {
+      title: '농장일기',
+      url: '/posts',
+    },
+    {
+      title: '공동구매',
+      url: '/groupbuy',
+    },
+  ];
+  const MenuIcons = [
+    {
+      icons: <AiOutlineHeart />,
+      title: '찜목록',
+      url: '/like',
+      color: '#FF6D59',
+    },
+    {
+      icons: <AiOutlineShoppingCart />,
+      title: '장바구니',
+      url: '/cart',
+      color: '#40AA54',
+    },
+  ];
   return (
     <Navbar>
-      <BlackLogoImg
-        src={onandthefarmlogo}
-        alt="onandthefarmlogo"
-      ></BlackLogoImg>
+      <LogoImg src={onandthefarmlogo} alt="onandthefarmlogo"></LogoImg>
       <NavbarMenu>
         {MenuItems.map((item, index) => {
           return (
@@ -29,23 +60,24 @@ export default function MainNavbar(props) {
           );
         })}
       </NavbarMenu>
-      <NavbarMenu>
+      <NavbarIcons>
         {MenuIcons.map((item, index) => {
           return (
             <Link key={index} to={item.url}>
-              <li>
-                <NavbarMenuPtag>{item.title}</NavbarMenuPtag>
-              </li>
+              <NavbarIcon color={item.color}>{item.icons}</NavbarIcon>
             </Link>
           );
         })}
-      </NavbarMenu>
+      </NavbarIcons>
       <NavbarUser>
-      <Link to='/mypage'>
-              <li>
-                <NavbarMenuPtag>안녕하세요 진영님</NavbarMenuPtag>
-              </li>
-            </Link>
+        <Link to="/seller">
+          <NavbarSeller>판매자센터 바로가기</NavbarSeller>
+        </Link>
+        <Link to="/login">
+          <li>
+            <NavbarMenuPtag>로그인</NavbarMenuPtag>
+          </li>
+        </Link>
       </NavbarUser>
     </Navbar>
   );
