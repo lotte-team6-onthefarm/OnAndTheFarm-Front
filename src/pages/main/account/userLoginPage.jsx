ad957732feb328787e9ffe8c763de5f37101bc41
이석원 <clalsw@seokwon-MacBookPro.local>
2022-10-05 14:41:16 +0900

    feat/소셜로그인페이지

--- src/pages/main/account/userLoginPage.jsx
+++ src/pages/main/account/userLoginPage.jsx
@@ -4,14 +4,20 @@ import { StyledBoxDiv, SocialLoginButton } from './userLoginPage.style';
 import { RiKakaoTalkFill } from 'react-icons/ri';
 import { SiNaver } from 'react-icons/si';
 import { AiFillGoogleCircle } from 'react-icons/ai';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function UserLoginPage() {
-
+  const navigate = useNavigate();
+  const REACT_APP_KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
+  const loginKakao = () => {
+    const kakao_url = `https://kauth.kakao.com/oauth/authorize?client_id=${REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=http://localhost:3000/login/success/kakao&response_type=code`;
+    window.location.href = kakao_url
+  };
   return (
     <StyledBoxDiv>
       <WellcomeMessage message="OnAndTheFarm에 오신것을 환영합니다." />
       <p style={{margin:"20px 0"}}>소셜로그인으로 간편하게 서비스를 이용해 보세요</p>
-      <SocialLoginButton color="#fee500" width="100%"><RiKakaoTalkFill /> <span>&nbsp; &nbsp; 카카오로 로그인</span></SocialLoginButton>
+      <SocialLoginButton color="#fee500" width="100%" onClick={loginKakao}><RiKakaoTalkFill /> <span>&nbsp; &nbsp; 카카오로 로그인</span></SocialLoginButton>
       <SocialLoginButton color="#21c603" width="100%"><SiNaver /> <span>&nbsp; &nbsp; 네이버로 로그인</span></SocialLoginButton>
       <SocialLoginButton color="#3c5a9a" width="100%"><AiFillGoogleCircle /> <span>&nbsp; &nbsp; 구글로 로그인</span></SocialLoginButton>
       
