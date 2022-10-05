9cd25afda535b38c13a9647180da0fb60cca5c17
이석원 <clalsw@seokwon-MacBookPro.local>
2022-10-05 14:43:13 +0900

    feat/유저초기 회원가입

---
+++ src/pages/main/account/userSignupPage.jsx
@@ -0,0 +1,141 @@
+import { React, useState } from 'react';
+import { useMutation } from 'react-query';
+import { postUserSignup } from '../../../apis/user/account';
+import WellcomeMessage from '../../../components/main/WellcomeMessage';
+import Input from '../../../components/common/Input';
+import { StyledBoxDiv, StyledRowDiv, StyledFind } from './userSignupPage.style';
+import { Button } from '../../../components/common/Button';
+import { useLocation } from 'react-router-dom';
+
+export default function UserSignupPage() {
+  const [userEmail, setUserEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [userPhone, setUserPhone] = useState('');
+  const [userPostCode, setUserPostCode] = useState('');
+  const [userAddress, setUserAddress] = useState('');
+  const [userDetailAddress, setUserDetailAddress] = useState('');
+  const [userGender, setUserGender] = useState('');
+  const [userBirthday, setUserBirthday] = useState('');
+
+  const { state } = useLocation();
+  const test = () => {
+    let data = {
+      "userName":userName,
+      "userPhone":userPhone,
+      "userZipcode":userPostCode,
+      "userAddress":userAddress,
+      "userAddressDetail":userDetailAddress,
+      "userSex":0,
+      "userBirthday":userBirthday
+  };
+    console.log(data)
+    console.log(state);
+    console.log('Tesa');
+    signupUser(data);
+  };
+
+  const { mutate: signupUser, isLoading: issignupUser } = useMutation(
+    postUserSignup,
+    {
+      onSuccess: res => {
+        console.log(res, '성공');
+      },
+      onError: () => {
+        console.log('에러');
+      },
+    },
+  );
+  return (
+    <StyledBoxDiv>
+      <WellcomeMessage message="OnAndTheFarm 회원가입 페이지" />
+      <StyledRowDiv position="start">
+        <Input
+          value={userEmail}
+          onChange={e => setUserEmail(e.target.value)}
+          label="이메일"
+          placeholder="test@email.com"
+          id="email"
+          type="email"
+        />
+      </StyledRowDiv>
+      <Input
+        value={userName}
+        onChange={e => setUserName(e.target.value)}
+        label="이름"
+        placeholder="홍길동"
+        id="name"
+        type="text"
+      />
+      <StyledRowDiv position="start">
+        <Input
+          value={userPhone}
+          onChange={e => setUserPhone(e.target.value)}
+          label="전화번호"
+          placeholder="010-1234-5678"
+          id="phoe"
+          type="text"
+        />
+        <Button
+          text="중복확인"
+          color="#3288E5"
+          margin="auto auto 20px"
+          width="150px"
+        ></Button>
+      </StyledRowDiv>
+      <StyledRowDiv position="start">
+        <Input
+          value={userPostCode}
+          onChange={e => setUserPostCode(e.target.value)}
+          label="우편번호"
+          placeholder="012345"
+          id="postcode"
+          type="text"
+        />
+        <Button
+          text="우편번호 찾기"
+          color="#3288E5"
+          margin="auto auto 20px"
+          width="150px"
+        ></Button>
+      </StyledRowDiv>
+      <Input
+        value={userAddress}
+        onChange={e => setUserAddress(e.target.value)}
+        label="주소"
+        placeholder="서울시 서초구 서초대로 74길"
+        id="address"
+        type="text"
+      />
+      <Input
+        value={userDetailAddress}
+        onChange={e => setUserDetailAddress(e.target.value)}
+        label="상세주소"
+        placeholder="3층"
+        id="detailaddress"
+        type="text"
+      />
+      <Input
+        value={userGender}
+        onChange={e => setUserGender(e.target.value)}
+        label="성별"
+        placeholder="남/여"
+        id="gender"
+        type="text"
+      />
+      <Input
+        value={userBirthday}
+        onChange={e => setUserBirthday(e.target.value)}
+        label="생일"
+        placeholder="2000-01-01"
+        id="birthday"
+        type="text"
+      />
+      <Button
+        text="회원가입"
+        color="#3288E5"
+        width="150px"
+        onClick={test}
+      ></Button>
+    </StyledBoxDiv>
+  );
+}
