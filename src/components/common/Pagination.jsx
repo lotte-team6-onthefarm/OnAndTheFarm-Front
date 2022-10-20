20c213948c0dfae547332f4f8a205bb879041c9f
dmstjd3256 <dmstjd3256@naver.com>
2022-10-20 20:50:40 +0900

    product pagenation

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -6,10 +6,10 @@ export default function Pagination(props) {
 
   // function
   const minus = e => {
-    if(Number(number) - 1<1){
-      alert('숫자는 정수만 입력해주세요')
-      setNumber(1)
-      return
+    if (Number(number) - 1 < 1) {
+      alert('숫자는 정수만 입력해주세요');
+      setNumber(1);
+      return;
     }
     setNumber(Number(number) - 1);
   };
@@ -18,25 +18,33 @@ export default function Pagination(props) {
   };
   const change = e => {
     // 숫자만 입력받기위한 정규식
-    let number = Number(e.target.value.replace(/[^0-9]/g, ""));
+    let number = Number(e.target.value.replace(/[^0-9]/g, ''));
     // 정수가 아닐경우
-    if(e.target.value<1){
-      alert('숫자는 정수만 입력해주세요')
-      setNumber(1)
-      return
+    if (e.target.value < 1) {
+      alert('숫자는 정수만 입력해주세요');
+      setNumber(1);
+      return;
     }
-    setNumber(number)
-  }
+    setNumber(number);
+  };
 
   // 숫자 변경시 올려주기
   useEffect(() => {
-    props.setPage(number)
+    props.setPage(number);
   }, [number, props]);
   return (
     <CounterDiv>
-      <CounterButton onClick={minus}><span>-</span></CounterButton>
-      <CounterNumber value={number} onChange={change}/>
-      <CounterButton onClick={plus}><span>+</span></CounterButton>
+      <CounterButton onClick={minus}>
+        <span>◀</span>
+      </CounterButton>
+      <CounterNumber value={1} onChange={change} />
+      <CounterNumber value={2} onChange={change} />
+      <CounterNumber value={3} onChange={change} />
+      <CounterNumber value={4} onChange={change} />
+      <CounterNumber value={5} onChange={change} />
+      <CounterButton onClick={plus}>
+        <span>▶</span>
+      </CounterButton>
     </CounterDiv>
   );
 }
