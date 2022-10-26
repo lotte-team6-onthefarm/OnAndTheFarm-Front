2a9baf835fdfd07a50640cf318c0cc2f7b5fd41e
이석원 <clalsw@seokwon-MacBookPro.local>
2022-10-26 20:09:03 +0900

    feat/무한스크롤

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Background, LoadingText } from './Loading.style';
 
 export default function Loading(props) {
+  console.log(props.loading)
   return (
-    <Background>
+    <Background loading={props.loading}>
       <LoadingText>잠시만 기다려 주세요.</LoadingText>
     </Background>
   );
