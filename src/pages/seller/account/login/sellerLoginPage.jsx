5ece643f79f3b8eccb3a033950c442261855941b
이석원 <clalsw@seokwon-MacBookPro.local>
2022-10-28 16:39:11 +0900

    feat/로그인시 토근과 롤

--- src/pages/seller/account/login/sellerLoginPage.jsx
+++ src/pages/seller/account/login/sellerLoginPage.jsx
@@ -34,18 +34,19 @@ export default function SellerLoginPage() {
       sellerLogin({ email: email, password: password });
     }
   };
-
   const { mutate: sellerLogin } = useMutation(postSellerlogin, {
     onSuccess: res => {
       if (localStorage.getItem('token') !== undefined) {
         // 셀러 로그인 시 유저 정보 있으면 셀러 토큰 제거
         localStorage.removeItem('token');
+        localStorage.removeItem('role');
       }
       if (localStorage.getItem('recoil-persist') !== undefined) {
         // 셀러 로그인 시 유저 정보가 있으면 제거
         localStorage.removeItem('recoil-persist');
       }
-      localStorage.setItem('sellerToken', res.data.token.token);
+      localStorage.setItem('token', res.data.token.token);
+      localStorage.setItem('role', 'seller');
       document.location.href = '/seller';
     },
     onError: () => {
