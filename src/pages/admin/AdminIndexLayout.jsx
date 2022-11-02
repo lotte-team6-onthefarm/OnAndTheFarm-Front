d4e48312cb71905c542169bf4c76383c9aed38b4
dmstjd3256 <dmstjd3256@naver.com>
2022-11-02 16:23:13 +0900

    feat/전시 navbar 설정

---
+++ src/pages/admin/AdminIndexLayout.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AdminNavbar from '../../components/admin/navbar/AdminNavbar';
+import { MainWrapper } from '../../components/seller/common/Box.style';
+import AccountList from './account/accountList/AccountList';
+import AddAccount from './account/addAccount/AddAccount';
+import AddModule from './module/addModule/AddModule';
+import ModuleList from './module/moduleList/ModuleList';
+import PageLayout from './PageLayout';
+
+export default function AdminIndexLayout() {
+  return (
+    <MainWrapper>
+      <AdminNavbar />
+      <Routes>
+        <Route path="/" element={<AccountList />} />
+        <Route path="/account/add" element={<AddAccount />} />
+        <Route path="/module" element={<ModuleList />} />
+        <Route path="/module/add" element={<AddModule />} />
+        <Route path="/pageLayout/add" element={<PageLayout />} />
+      </Routes>
+    </MainWrapper>
+  );
+}
--- src/pages/seller/account/login/sellerLoginPage.jsx
+++ src/pages/seller/account/login/sellerLoginPage.jsx
@@ -36,18 +36,22 @@ export default function SellerLoginPage() {
   };
   const { mutate: sellerLogin } = useMutation(postSellerlogin, {
     onSuccess: res => {
-      if (localStorage.getItem('token') !== undefined) {
-        // 셀러 로그인 시 유저 정보 있으면 셀러 토큰 제거
-        localStorage.removeItem('token');
-        localStorage.removeItem('role');
+      if (email === 'admin@admin.com') {
+        document.location.href = '/admin';
+      } else {
+        if (localStorage.getItem('token') !== undefined) {
+          // 셀러 로그인 시 유저 정보 있으면 셀러 토큰 제거
+          localStorage.removeItem('token');
+          localStorage.removeItem('role');
+        }
+        if (localStorage.getItem('recoil-persist') !== undefined) {
+          // 셀러 로그인 시 유저 정보가 있으면 제거
+          localStorage.removeItem('recoil-persist');
+        }
+        localStorage.setItem('token', res.data.token.token);
+        localStorage.setItem('role', 'seller');
+        document.location.href = '/seller';
       }
-      if (localStorage.getItem('recoil-persist') !== undefined) {
-        // 셀러 로그인 시 유저 정보가 있으면 제거
-        localStorage.removeItem('recoil-persist');
-      }
-      localStorage.setItem('token', res.data.token.token);
-      localStorage.setItem('role', 'seller');
-      document.location.href = '/seller';
     },
     onError: () => {
       alert('아이디 또는 비밀번호가 일치하지 않습니다.');
