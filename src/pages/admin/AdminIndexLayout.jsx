b441333a4988feca08f42ff8e0e20872539154fa
clalsw <68373904+clalsw@users.noreply.github.com>
2022-11-03 17:43:37 +0900

    Feat/lsw 배너,모듈,뱃지등록, 리스트 (#116)
    * feat/모듈리스트
    
    * feat/배너,뱃지 등록
    
    Co-authored-by: 이석원 <clalsw@seokwon-MacBookPro.local>

--- src/pages/admin/AdminIndexLayout.jsx
+++ src/pages/admin/AdminIndexLayout.jsx
@@ -4,6 +4,8 @@ import AdminNavbar from '../../components/admin/navbar/AdminNavbar';
 import { MainWrapper } from '../../components/seller/common/Box.style';
 import AccountList from './account/accountList/AccountList';
 import AddAccount from './account/addAccount/AddAccount';
+import AddBadge from './data/badge/AddBadge';
+import AddBanner from './data/banner/AddBanner';
 import AddModule from './module/addModule/AddModule';
 import ModuleList from './module/moduleList/ModuleList';
 import PageLayout from './PageLayout';
@@ -17,6 +19,8 @@ export default function AdminIndexLayout() {
         <Route path="/account/add" element={<AddAccount />} />
         <Route path="/module" element={<ModuleList />} />
         <Route path="/module/add" element={<AddModule />} />
+        <Route path="/data/badge/add" element={<AddBadge />} />
+        <Route path="/data/banner/add" element={<AddBanner />} />
         <Route path="/pageLayout/add" element={<PageLayout />} />
       </Routes>
     </MainWrapper>
