98eb047a0f2c2aa8f83a017c1428dd0c4f40e657
clalsw <68373904+clalsw@users.noreply.github.com>
2022-11-15 17:14:53 +0900

    feat/리프레쉬토큰 (#155)
    Co-authored-by: 이석원 <clalsw@seokwon-MacBookPro.local>

--- src/apis/index.js
+++ src/apis/index.js
@@ -5,8 +5,8 @@ const SELLER_BASE_URL = process.env.REACT_APP_SELLER_BASE_URL;
 const ADMIN_BASE_URL = process.env.REACT_APP_ADMIN_BASE_URL;
 const USER_BASE_URL = process.env.REACT_APP_USER_BASE_URL;
 
-const ACCESS_TOKEN = localStorage.getItem('token');
-const JWTapiSeller = axios.create({
+let ACCESS_TOKEN = localStorage.getItem('token');
+let JWTapiSeller = axios.create({
   baseURL: SELLER_BASE_URL,
   headers: {
     contentType: 'application/json',
@@ -14,7 +14,7 @@ const JWTapiSeller = axios.create({
   },
 });
 
-const JWTapiAdmin = axios.create({
+let JWTapiAdmin = axios.create({
   baseURL: ADMIN_BASE_URL,
   headers: {
     contentType: 'application/json',
@@ -72,6 +72,112 @@ const getProductDetailImg = async data => {
   const response = await BaseApi.get(data);
   return response.data;
 };
+
+JWTapiSeller.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (err) {
+    const originalConfig = err.config;
+
+    if (err.response && err.response.status === 406) {
+      const refreshToken = localStorage.getItem('refreshToken');
+      try {
+        // 리프레시 토큰으로 access토큰 받아오기
+        const data = await axios({
+          url: `${SELLER_BASE_URL}members/refresh`,
+          method: 'post',
+          data: {
+            accessToken: ACCESS_TOKEN,
+            refreshToken: refreshToken,
+          },
+        });
+        if (data) {
+          localStorage.removeItem('refreshToken');
+          localStorage.removeItem('token');
+          localStorage.setItem('token', data.data.data.token.token);
+          localStorage.setItem(
+            'refreshToken',
+            data.data.data.token.refreshToken,
+          );
+          ACCESS_TOKEN = data.data.data.token.token;
+
+          // api update
+          JWTapiSeller = axios.create({
+            baseURL: SELLER_BASE_URL,
+            headers: {
+              contentType: 'application/json',
+              Authorization: ACCESS_TOKEN,
+            },
+          });
+          // 새 토큰으로 재요청
+          originalConfig.headers['Authorization'] = data.data.data.token.token;
+          return await JWTapiSeller.request(originalConfig);
+        }
+      } catch (err) {
+        console.log('토큰 갱신 에러');
+      }
+      return Promise.reject(err);
+    }
+    return Promise.reject(err);
+  },
+);
+
+
+JWTapiAdmin.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (err) {
+    const originalConfig = err.config;
+
+    if (err.response && err.response.status === 406) {
+      const refreshToken = localStorage.getItem('refreshToken');
+      try {
+        // 리프레시 토큰으로 access토큰 받아오기
+        const data = await axios({
+          url: `${ADMIN_BASE_URL}members/refresh`,
+          method: 'post',
+          data: {
+            accessToken: ACCESS_TOKEN,
+            refreshToken: refreshToken,
+          },
+        });
+        if (data) {
+          localStorage.removeItem('refreshToken');
+          localStorage.removeItem('token');
+          localStorage.setItem('token', data.data.data.token.token);
+          localStorage.setItem(
+            'refreshToken',
+            data.data.data.token.refreshToken,
+          );
+          ACCESS_TOKEN = data.data.data.token.token;
+
+          // api update
+          JWTapiAdmin = axios.create({
+            baseURL: ADMIN_BASE_URL,
+            headers: {
+              contentType: 'application/json',
+              Authorization: ACCESS_TOKEN,
+            },
+          });
+          // 새 토큰으로 재요청
+          originalConfig.headers['Authorization'] = data.data.data.token.token;
+          return await JWTapiAdmin.request(originalConfig);
+        }
+      } catch (err) {
+        console.log('토큰 갱신 에러');
+      }
+      return Promise.reject(err);
+    }
+    return Promise.reject(err);
+  },
+);
+
+
+
+
+
 
 export {
   JWTapiSeller,
--- src/apis/user/index.js
+++ src/apis/user/index.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const USER_BASE_URL = process.env.REACT_APP_USER_BASE_URL;
 
-const ACCESS_TOKEN = localStorage.getItem('token');
-const JWTapiUser = axios.create({
+let ACCESS_TOKEN = localStorage.getItem('token');
+
+let JWTapiUser = axios.create({
   baseURL: USER_BASE_URL,
   headers: {
     contentType: 'application/json',
@@ -17,5 +18,55 @@ const ApiUser = axios.create({
     contentType: 'application/json',
   },
 });
+
+JWTapiUser.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (err) {
+    const originalConfig = err.config;
+
+    if (err.response && err.response.status === 406) {
+      const refreshToken = localStorage.getItem('refreshToken');
+      try {
+        // 리프레시 토큰으로 access토큰 받아오기
+        const data = await axios({
+          url: `${USER_BASE_URL}members/refresh`,
+          method: 'post',
+          data: {
+            accessToken: ACCESS_TOKEN,
+            refreshToken: refreshToken,
+          },
+        });
+        if (data) {
+          localStorage.removeItem('refreshToken');
+          localStorage.removeItem('token');
+          localStorage.setItem('token', data.data.data.token.token);
+          localStorage.setItem(
+            'refreshToken',
+            data.data.data.token.refreshToken,
+          );
+          ACCESS_TOKEN = data.data.data.token.token;
+
+          // api update
+          JWTapiUser = axios.create({
+            baseURL: USER_BASE_URL,
+            headers: {
+              contentType: 'application/json',
+              Authorization: ACCESS_TOKEN,
+            },
+          });
+          // 새 토큰으로 재요청
+          originalConfig.headers['Authorization'] = data.data.data.token.token;
+          return await JWTapiUser.request(originalConfig);
+        }
+      } catch (err) {
+        console.log('토큰 갱신 에러');
+      }
+      return Promise.reject(err);
+    }
+    return Promise.reject(err);
+  },
+);
 
 export { JWTapiUser, ApiUser };
