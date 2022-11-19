3b0e8b31ca112368d07615243ec98bc523502b01
clalsw <68373904+clalsw@users.noreply.github.com>
2022-11-19 23:38:14 +0900

    feat/로그아웃 토큰 (#175)
    Co-authored-by: 이석원 <clalsw@seokwon-MacBookPro.local>

--- src/apis/index.js
+++ src/apis/index.js
@@ -6,6 +6,8 @@ const ADMIN_BASE_URL = process.env.REACT_APP_ADMIN_BASE_URL;
 const USER_BASE_URL = process.env.REACT_APP_USER_BASE_URL;
 
 let ACCESS_TOKEN = localStorage.getItem('token');
+let REFRESH_TOKEN = localStorage.getItem('refreshToken');
+
 let JWTapiSeller = axios.create({
   baseURL: SELLER_BASE_URL,
   headers: {
@@ -19,6 +21,24 @@ let JWTapiAdmin = axios.create({
   headers: {
     contentType: 'application/json',
     Authorization: ACCESS_TOKEN,
+  },
+});
+
+let JWTapiLogoutSeller = axios.create({
+  baseURL: SELLER_BASE_URL,
+  headers: {
+    contentType: 'application/json',
+    Authorization: ACCESS_TOKEN,
+    refresh: REFRESH_TOKEN,
+  },
+});
+
+let JWTapiLogoutAdmin = axios.create({
+  baseURL: ADMIN_BASE_URL,
+  headers: {
+    contentType: 'application/json',
+    Authorization: ACCESS_TOKEN,
+    refresh: REFRESH_TOKEN,
   },
 });
 
@@ -74,6 +94,104 @@ const getProductDetailImg = async data => {
 };
 
 JWTapiSeller.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (err) {
+    const originalConfig = err.config;
+
+    if (err.response && err.response.status === 406) {
+      const refreshToken = localStorage.getItem('refreshToken');
+      try {
+        // 리프레시 토큰으로 access토큰 받아오기
+        const data = await axios({
+          url: `${SELLER_BASE_URL}members/refresh`,
+          method: 'post',
+          data: {
+            accessToken: ACCESS_TOKEN,
+            refreshToken: refreshToken,
+          },
+        });
+        if (data) {
+          localStorage.removeItem('refreshToken');
+          localStorage.removeItem('token');
+          localStorage.setItem('token', data.data.data.token.token);
+          localStorage.setItem(
+            'refreshToken',
+            data.data.data.token.refreshToken,
+          );
+          ACCESS_TOKEN = data.data.data.token.token;
+
+          // api update
+          JWTapiSeller = axios.create({
+            baseURL: SELLER_BASE_URL,
+            headers: {
+              contentType: 'application/json',
+              Authorization: ACCESS_TOKEN,
+            },
+          });
+          // 새 토큰으로 재요청
+          originalConfig.headers['Authorization'] = data.data.data.token.token;
+          return await JWTapiSeller.request(originalConfig);
+        }
+      } catch (err) {
+        console.log('토큰 갱신 에러');
+      }
+      return Promise.reject(err);
+    }
+    return Promise.reject(err);
+  },
+);
+JWTapiLogoutSeller.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (err) {
+    const originalConfig = err.config;
+
+    if (err.response && err.response.status === 406) {
+      const refreshToken = localStorage.getItem('refreshToken');
+      try {
+        // 리프레시 토큰으로 access토큰 받아오기
+        const data = await axios({
+          url: `${SELLER_BASE_URL}members/refresh`,
+          method: 'post',
+          data: {
+            accessToken: ACCESS_TOKEN,
+            refreshToken: refreshToken,
+          },
+        });
+        if (data) {
+          localStorage.removeItem('refreshToken');
+          localStorage.removeItem('token');
+          localStorage.setItem('token', data.data.data.token.token);
+          localStorage.setItem(
+            'refreshToken',
+            data.data.data.token.refreshToken,
+          );
+          ACCESS_TOKEN = data.data.data.token.token;
+
+          // api update
+          JWTapiSeller = axios.create({
+            baseURL: SELLER_BASE_URL,
+            headers: {
+              contentType: 'application/json',
+              Authorization: ACCESS_TOKEN,
+            },
+          });
+          // 새 토큰으로 재요청
+          originalConfig.headers['Authorization'] = data.data.data.token.token;
+          return await JWTapiSeller.request(originalConfig);
+        }
+      } catch (err) {
+        console.log('토큰 갱신 에러');
+      }
+      return Promise.reject(err);
+    }
+    return Promise.reject(err);
+  },
+);
+JWTapiLogoutAdmin.interceptors.response.use(
   function (response) {
     return response;
   },
@@ -181,8 +299,10 @@ JWTapiAdmin.interceptors.response.use(
 
 export {
   JWTapiSeller,
+  JWTapiLogoutSeller,
   ApiSeller,
   JWTapiAdmin,
+  JWTapiLogoutAdmin,
   JWTapiSellertoUser,
   CommonJWTapi,
   getProductDetailImg,
