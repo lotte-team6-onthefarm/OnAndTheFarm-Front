992fa093812df3e6cb2f32913b8618642b14e4b8
dmstjd3256 <dmstjd3256@naver.com>
2022-11-21 11:30:28 +0900

    feat/SNS 로그인 유효성 체크

--- src/components/sns/feed/FeedWriter.jsx
+++ src/components/sns/feed/FeedWriter.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useMutation } from 'react-query';
 import { Link, useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 import { postAddFollow, putCancelFollow } from '../../../apis/sns/profile';
 import { FeedWriterWrapper } from '../../../pages/sns/feed/Feed.styled';
+import { preLoginUrl } from '../../../recoil';
 
 export default function FeedWriter(props) {
+  const [preUrl, setPreUrl] = useRecoilState(preLoginUrl);
+  const userToken = localStorage.getItem('token');
   const navigate = useNavigate();
   const { mutate: addFollow, isLoading: isPostAddFollow } = useMutation(
     postAddFollow,
@@ -44,23 +48,41 @@ export default function FeedWriter(props) {
         <></>
       ) : props.followStatus ? (
         <button
-          onClick={() =>
+          onClick={() => {
+            // 로그인 페이지 보내주기
+            if (userToken === null) {
+              // 로그인 안했을 때
+              setPreUrl(window.location.href);
+              alert('로그인이 필요한 서비스 입니다.');
+              navigate('/login');
+              return;
+            }
+
             cancelFollow({
               followerMemberId: props.memberId,
               followerMemberRole: props.memberRole,
-            })
-          }
+            });
+          }}
         >
           &nbsp;&nbsp;팔로잉
         </button>
       ) : (
         <button
-          onClick={() =>
+          onClick={() => {
+            // 로그인 페이지 보내주기
+            if (userToken === null) {
+              // 로그인 안했을 때
+              setPreUrl(window.location.href);
+              alert('로그인이 필요한 서비스 입니다.');
+              navigate('/login');
+              return;
+            }
+
             addFollow({
               followerMemberId: props.memberId,
               followerMemberRole: props.memberRole,
-            })
-          }
+            });
+          }}
         >
           &nbsp;&nbsp;팔로우
         </button>
